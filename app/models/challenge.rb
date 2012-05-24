06bd2a708d82198dfd05d03eaa835f32b8774dce
Oto Brglez <otobrglez@gmail.com>
2012-05-24 13:29:29 +0200

    Doing some BL changes

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -4,31 +4,22 @@ class Challenge
 
   belongs_to :from, class_name: "User"
 
-  field :to_fb_uids, type: Array
-
   field :title
   field :description
   field :punishment
   field :reward
 
+  field :public, type: Integer, default: 0
   field :deleted, type: Integer, default: 0
-  field :for_all, type: Integer, default: 0
-
-  field :status, type: String, default: "created"
-  index :status
-
-
   field :deadline, type: DateTime
 
-  validates_presence_of :deadline
-  validates_presence_of :status
-  validates_presence_of :title
   validates_presence_of :from
+  validates_presence_of :title
+  validates_presence_of :deadline
 
   validate do |ch|
 	ch.must_be_punishment_or_reward
 	ch.must_be_in_future
-	ch.must_have_to_fb_uids
   end
 
   def to_s
@@ -54,11 +45,6 @@ class Challenge
   	end
   end
 
-  # Is this challange for everybody?
-  def for_all?
-  	self.for_all == 1
-  end
-
   # You must set at least one FB user if its not for all
   def must_have_to_fb_uids
   	if not for_all? and (self.to_fb_uids.nil? or self.to_fb_uids.empty?)
@@ -66,67 +52,7 @@ class Challenge
   	end
   end
 
-  def created?
-  	self.status == "created"
-  end
-
-  def accept
-  	self.status = "accepted"
-  end
-
-  def accepted?
-  	self.status == "accepted"
-  end
-
-  def reject
-  	self.status = "rejected"
-  end
-
-  def rejected?
-  	self.status == "rejected"
-  end
-
-  def answer
-  	self.status = "answered"
-  end
-
-  def answered?
-  	self.status == "answered"
-  end
-
-  def complete
-  	self.status = "completed"
-  end
-
-  def completed?
-  	self.status == "completed"
-  end
-
-  def fail
-  	self.status = "failed"
-  end
-
-  def failed?
-  	self.status == "failed"
-  end
-
-  def to= user
-  	if self.to_fb_uids.nil? or self.to_fb_uids.empty?
-  		self.to_fb_uids=[user.fb_uid]
-  	elsif not(self.to_fb_uids.include? user.fb_uid)
-  		self.to_fb_uids << user.fb_uid
-  	end
-  end
-
-  def to
-  	return self.to_fb_uids.first if not(self.to_fb_uids.nil?) and self.to_fb_uids.size == 1
-  	return self.to_fb_uids if not(self.to_fb_uids.nil?)
-  	return []
-  end
-
-  def for_many?
-  	not(self.to_fb_uids.nil?) and self.to_fb_uids.size > 1
-  end
+=begin
 
   # Order by updated_at
   default_scope order_by([:updated_at,:desc])
@@ -149,5 +75,6 @@ class Challenge
   scope :accepted, ->(){
   	with_status(:accepted)
   }
+=end
 
 end
